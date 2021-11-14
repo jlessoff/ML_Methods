8beb83029f7ad130f82fad2aa75263f340240523
jlessoff <jlessoff@gmail.com>
2021-11-14 17:46:07 +0100

    Editing in class submission to add question names and titles to graphs

--- Decision_Tree_Classification.py
+++ Decision_Tree_Classification.py
@@ -1,35 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import mean_squared_error
 from sklearn.metrics import accuracy_score
 
 
-#question 1: load the rice data set and prepare it for machine learning application using sample evaluation (with train-test-split).  keep 30%
+#Question 1 Load the rice data set and prepare it for a machine learning application using split sample evaluation (with the train_test_split function of Scikit-learn). Keep 30 % of the date set for the final evaluation.
 rice=pd.read_csv("rice.csv")
 rice_X=rice.drop(columns="CLASS")
 rice_Y=rice['CLASS']
-#get class names
+#Variable names
 rice_class = rice_Y.unique()
 rice_features = rice_X.columns
-#split the dataset
+#Split the dataset
 
 from sklearn.model_selection import train_test_split
 r_X_tr, r_X_test, r_Y_tr, r_Y_test=train_test_split(rice_X,rice_Y,
                                                     train_size=0.7, random_state=42)
+#Question 2 Compute the pruning summary on the training set for the rice data set and plot the evolution of the impurity on the training as a function of the alpha parameter.
 cl_model=DecisionTreeClassifier( random_state=42)
 cl_model.fit(r_X_tr,r_Y_tr)
 tree_prediction_test=cl_model.predict(r_X_test)
-
 print(tree_prediction_test)
-# question 2: compute the pruning summary on the training set for the rice data set and plot the evolution of the impurity as a function of the alpha param
+#Plot the evolution of the impurity on the training as a function of the alpha parameter.
 path=cl_model.cost_complexity_pruning_path(r_X_tr,r_Y_tr)
-print(path)
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
-print(ccp_alphas)
-print(impurities)
-#plot the evolution of the impurity on the training as a function of the alpha parameter
 plt.plot(ccp_alphas,impurities)
+plt.title("Accuracy and Impurity Tradeoff")
 plt.show()
 
 #Question 3 Using a loop, compute all the pruned trees as well as their accuracy on both the training set and the test set.
@@ -49,8 +45,11 @@ for i in range(0,len(ccp_alphas)):
 #Plot the results and comment them.
 print(acc_test)
 plt.plot(acc_test)
+plt.title("Accuracy Plot for Test Data")
 plt.show()
 
 plt.plot(acc_train)
+plt.title("Accuracy Plot for Training Data")
 plt.show()
-##ACCURACY INCREASES IN TEST AS THERE IS MORE PRUNING, AND DECREASES IN TRAINING WHEN THERE IS MORE PRUNING.  THIS MAKES SENSE BECAUSE PRUNING WILL HELP PREVENT OVERFITTING, LEADING TO BETTER RESULTS FOR THE TEST SET.
+#Pruning can prevent overfitting.  This will cause accuracy to increase in test predictions as there is more pruning, and  accuracy to decrease in training predictions when there is more pruning.
+print('Pruning can prevent overfitting.  This will cause accuracy to increase in test predictions as there is more pruning, and  accuracy to decrease in training predictions when there is more pruning.')
--- Decision_Trees.py
+++ Decision_Trees.py
@@ -2,8 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import accuracy_score
-
 
 # Question 1: Load wine data set and prepare for ML.  Keep 30% of data set for final evaluation
 #import data
@@ -12,9 +10,6 @@ wines=pd.read_csv('winequality-red.csv',delimiter=',',skiprows=1)
 wines_X=wines[wines.columns[1:12]]
 #target variable
 wines_Y=wines['quality']
-
-
-
 #split test, train
 from sklearn.model_selection import train_test_split
 X_tr, X_test, Y_tr, Y_test=train_test_split(wines_X,wines_Y,
@@ -26,32 +21,33 @@ regr_1.fit(X_tr,Y_tr)
 # Predict
 y_1 = regr_1.predict(X_test)
 print(y_1)
-# Plot the results
-#print(y_1)
+
+
 #accuracy check using MSE
 mse = mean_squared_error(Y_test, y_1)
 print(mse)
 
-
-#is this overfitting?  a good way to check: compare error from training set and test set.
-
 y_training_prediction=regr_1.predict(X_tr)
 mse_train = mean_squared_error(Y_tr, y_training_prediction)
 print(mse_train)
-###Yes, it is overfitting because the training MSE is zero and the test MSE is high###
-print("Yes, it is overfitting becaus the training MSE is zero and the test MSE is high")
+
 
 #graphical representation
+# Plot the results
 X_AXIS = range(len(Y_test))
 plt.plot(X_AXIS, Y_test, label="original")
 plt.plot(X_AXIS, y_1, label="predicted")
+plt.legend()
 plt.title("Wine test and predicted data")
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+
 plt.show()
 
+#Question 3: Assess whether the decision tree has overfitted the data.
+###Yes, it is overfitting because the training MSE is zero and the test MSE is high###
+print("Yes, it is overfitting becaus the training MSE is zero and the test MSE is high")
 
-##Create a loop that learns a series of trees with larger values for min_samp and collect the MSE of trees on learning set and test set
+
+#Question 4:  Use a loop to learn a series of trees with larger values for min_samples_splitand collect the MSE of trees on the learning set and on the test set. Draw the corresponding curve
 mse_test=[]
 mse_learn=[]
 for i in range(2,100):
@@ -65,17 +61,21 @@ for i in range(2,100):
     y_training_prediction = regr_1.predict(X_tr)
     mse_train = mean_squared_error(Y_tr, y_training_prediction)
     mse_learn.append(mse_train)
-#printing MSE for test and learning set
 
+#printing MSE for test and learning set
 print(mse_test)
 print(mse_learn)
+
+#Plotting curve for test and learning data
 plt.plot(mse_test)
+plt.title("Test Mean Squared Error mean_sample_split")
 plt.show()
 
 plt.plot(mse_learn)
+plt.title("Training Mean Squared Error mean_sample_split")
 plt.show()
 
-#
+#Question 5 Test in the same way the effect of max_depth parameter.
 mse_test=[]
 mse_learn=[]
 for i in range(2,40):
@@ -92,34 +92,39 @@ for i in range(2,40):
     mse_learn.append(mse_train)
 print(mse_test)
 plt.plot(mse_test)
+plt.title("Training Mean Squared Error max_depth")
 plt.show()
 
 print(mse_learn)
 plt.plot(mse_learn)
+plt.title("Training Mean Squared Error max_depth")
 plt.show()
 
+#Pruning and directly stopping the tree will have similar results.  However, pruning will yield slightly better accuracy because early stopping could cause short-sightedness because there could be a large improvement at the end of the tree creation
+print("Pruning and directly stopping the tree will have similar results.  However, pruning will yield slightly better accuracy because early stopping could cause short-sightedness because there could be a large improvement at the end of the tree creation")
 
 
-
+#Question 5 Apply the same strategy to the wine data set from exercise 1 and compare the effects of pruning with those of controlling directly the stopping criteria.
 path=regr_1.cost_complexity_pruning_path(X_tr,Y_tr)
 ccp_alphas, impurities = path.ccp_alphas, path.impurities
 acc_test=[]
 acc_train=[]
+
 for alpha in range(0,len(ccp_alphas)):
     clf= DecisionTreeRegressor(random_state=42, ccp_alpha= ccp_alphas[alpha])
     clf.fit(X_tr, Y_tr)
     tree_prediction_test = clf.predict(X_test)
     accuracy_test=mean_squared_error(Y_test,tree_prediction_test)
-    #print(ccp_alphas[alpha])
     acc_test.append(accuracy_test)
 
     tree_prediction_train = clf.predict(X_tr)
     accuracy_train=mean_squared_error(Y_tr,tree_prediction_train)
     acc_train.append(accuracy_train)
 plt.plot(acc_test)
+plt.title("Accuracy Plot for Test Data")
 plt.show()
 
 plt.plot(acc_train)
+plt.title("Accuracy Plot for Training Data")
+
 plt.show()
-##PRUNING AND DIRECTLY STOPPING THE TREE WILL HAVE SIMILAR RESULTS.  HOWEVER, PRUNING SHOWS SLIGHTLY BETTER ACCURACY (BEING ABLE TO SEE THE ENTIRE ERROR BEFORE CUTTING BACK INSTEAD OF PREVENTING GROWTH).
-print("PRUNING AND DIRECTLY STOPPING THE TREE WILL HAVE SIMILAR RESULTS.  HOWEVER, PRUNING SHOWS SLIGHTLY BETTER ACCURACY (BEING ABLE TO SEE THE ENTIRE ERROR BEFORE CUTTING BACK INSTEAD OF PREVENTING GROWTH).")
