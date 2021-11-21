7c6f968ceb6a523cf606082aa461f941e34df8c8
jlessoff <jlessoff@gmail.com>
2021-11-21 17:38:17 +0100

    Fixing bugs

--- resampling_lab.py
+++ resampling_lab.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import statistics
-
-
-import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
 from sklearn.model_selection import cross_val_score
-import sklearn.metrics
 import seaborn as sns
 
 #import data
@@ -16,9 +12,10 @@ wines=pd.read_csv('winequality-red.csv',delimiter=',',skiprows=1)
 X=wines[wines.columns[1:12]]
 #target variable
 y=wines['quality']
+i_list=[]
+
 average_score=[]
 #question 4
-i_list=[]
 for i in range(2,100):
     kf = KFold(n_splits=5, shuffle=True, random_state=4)
     score = cross_val_score(DecisionTreeRegressor(min_samples_split=i, random_state=42), X, y, cv= kf, scoring='neg_mean_squared_error')
@@ -27,25 +24,20 @@ for i in range(2,100):
         df=(i,l)
         average_score.append(-l)
         i_list.append(i)
-print(average_score)
-print(i_list)
-print(len(average_score))
 sns.relplot(x=i_list, y=average_score, kind='line')
 import matplotlib.pyplot as plt
-plt.show()
+# plt.show()
 # QUESTION 5
-name=[]
+name_1=[]
+i_list_1=[]
 for i in range(0,20):
     kf = KFold(n_splits=5, shuffle=True, random_state=i)
     score = cross_val_score(DecisionTreeRegressor(min_samples_split=5, random_state=42), X, y, cv= kf, scoring='neg_mean_squared_error')
-    i_list.append((statistics.mean(-score)))
-    name.append('fold')
-print(i_list)
-average=statistics.mean(i_list)
-ste=statistics.stdev(i_list)
-print('average for mse',average)
-print('standarddev for mse', ste)
-
+    i_list_1.append((statistics.mean(-score)))
+    name_1.append('fold')
+average=statistics.mean(i_list_1)
+ste=statistics.stdev(i_list_1)
+print(i_list_1)
 X=wines[wines.columns[1:12]]
 #target variable
 y=wines['quality']
@@ -73,12 +65,12 @@ for i in range(2,100):
     # accuracy check using MSE
     mse1 = mean_squared_error(yval, yvalid)
     mse_val.append(mse1)
-print(mse_val)
 sns.relplot(x=range(2,100), y=(mse_val), kind='line')
 plt.show()
 
 ###QUESTION 5
-mse_val=[]
+i_list=[]
+name=[]
 for i in range(0, 100):
     from sklearn.model_selection import train_test_split
 
@@ -94,12 +86,27 @@ for i in range(0, 100):
     name.append("split")
 average=statistics.mean(mse_val)
 ste=statistics.stdev(mse_val)
-
-#question 5
+print(i_list)
+print(name)
+print(len(i_list))
+print(len(name))
+# # question 5
 dataframe=pd.DataFrame()
+print(dataframe)
 dataframe['name']=name
 dataframe['mse']=i_list
-sns.kdeplot(x=dataframe['name'], data=dataframe)
+print(dataframe)
+sns.kdeplot(data=dataframe)
+
+
+dataframe1=pd.DataFrame()
+print(dataframe1)
+dataframe1['name']=name_1
+dataframe1['mse']=i_list_1
+print(dataframe1)
+sns.kdeplot(data=dataframe1)
+plt.show()
+
 
 ##TEXT ANSWERS:
 print('Question 3: the results for the cross validation approach shows a more stable descent in MSE than for the test/train approach.  This would be recommended')
