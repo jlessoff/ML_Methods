414fe7e46f959dafcd750c6ab713cda47d4e89ef
jlessoff <jlessoff@gmail.com>
2021-12-01 10:34:56 +0100

    My second submission

--- resampling_lab.py
+++ resampling_lab.py
@@ -2,9 +2,11 @@ import pandas as pd
 import statistics
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.model_selection import cross_val_score
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 #import data
 wines=pd.read_csv('winequality-red.csv',delimiter=',',skiprows=1)
@@ -15,31 +17,28 @@ y=wines['quality']
 i_list=[]
 
 average_score=[]
-#question 4
+#question 4: loop through min sample splits with kfold validation
 for i in range(2,100):
-    kf = KFold(n_splits=5, shuffle=True, random_state=0)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     score = cross_val_score(DecisionTreeRegressor(min_samples_split=i, random_state=42), X, y, cv= kf, scoring='neg_mean_squared_error')
     list=[]
     for l in score:
         df=(i,l)
         average_score.append(-l)
         i_list.append(i)
-        print(i,l)
-sns.relplot(x=i_list, y=average_score, kind='line')
-import matplotlib.pyplot as plt
-# plt.show()
-# QUESTION 5
+plot1=sns.relplot(x=i_list, y=average_score, kind='line')
+plot1.set(xlabel='min sample split', ylabel='mse')
+plt.show()
+# QUESTION 5 loop through random states to show the stability
 name_1=[]
 i_list_1=[]
 for i in range(0,20):
-    kf = KFold(n_splits=5, shuffle=True, random_state=i)
-    score = cross_val_score(DecisionTreeRegressor(min_samples_split=5, random_state=4), X, y, cv= kf, scoring='neg_mean_squared_error')
-    print(score)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=i)
+    score = cross_val_score(DecisionTreeRegressor(min_samples_split=5, random_state=42), X, y, cv= kf, scoring='neg_mean_squared_error')
     for l in score:
         df=(i,l)
         i_list_1.append((statistics.mean(-score)))
         name_1.append('fold')
-        print(l)
 average=statistics.mean(i_list_1)
 ste=statistics.stdev(i_list_1)
 X=wines[wines.columns[1:12]]
@@ -57,22 +56,23 @@ X=wines[wines.columns[1:12]]
 y=wines['quality']
 from sklearn.model_selection import train_test_split
 Xtrain, Xval, ytrain, yval=train_test_split(X,y,
-                                                    train_size=0.8, random_state=70)
+                                                    train_size=0.8, random_state=42)
 
-##QUESTION 4
+#question 4: loop through min sample splits with kfold validation
 mse_val=[]
 for i in range(2,100):
-    regr_1 = DecisionTreeRegressor(min_samples_split=i,random_state=4)
+    regr_1 = DecisionTreeRegressor(min_samples_split=i,random_state=42)
     regr_1.fit(Xtrain, ytrain)
     # Predict
     yvalid = regr_1.predict(Xval)
     # accuracy check using MSE
     mse1 = mean_squared_error(yval, yvalid)
     mse_val.append(mse1)
-sns.relplot(x=range(2,100), y=(mse_val), kind='line')
+mse_plot=sns.relplot(x=range(2,100), y=(mse_val), kind='line')
+mse_plot.set(xlabel='min sample split', ylabel='mse')
 plt.show()
 
-###QUESTION 5
+# QUESTION 5 loop through random states to show the stability
 i_list=[]
 name=[]
 for i in range(0, 100):
@@ -80,7 +80,7 @@ for i in range(0, 100):
 
     Xtrain, Xval, ytrain, yval = train_test_split(X, y,
                                                   train_size=0.8, random_state=i)
-    regr_1 = DecisionTreeRegressor(min_samples_split=5, random_state=4)
+    regr_1 = DecisionTreeRegressor(min_samples_split=5, random_state=42)
     regr_1.fit(Xtrain, ytrain)
     # Predict
     yvalid = regr_1.predict(Xval)
@@ -91,18 +91,11 @@ for i in range(0, 100):
 average=statistics.mean(mse_val)
 ste=statistics.stdev(mse_val)
 # # question 5
-dataframe=pd.DataFrame()
-dataframe['name']=name
-dataframe['mse']=i_list
-sns.kdeplot(data=dataframe)
-
 dataframe1=pd.DataFrame()
 dataframe1['name']=name_1
 dataframe1['mse']=i_list_1
 sns.kdeplot(data=dataframe1, x=i_list_1 ,hue="name", legend=True)
 plt.show()
-print(dataframe)
-print(dataframe1)
 ##TEXT ANSWERS:
 print('Question 3: the results for the cross validation approach shows a more stable descent in MSE than for the test/train approach.  This would be recommended')
-print('Question 4: using cross validation results in a much more stable result than using test/train across many random states.  The MSE results obtained from test/train are very variable and depend a lot on the data that is split.  This is mitigated by breaking into 5 different folds')
+print('Question 4: After testing with several random states, it is clear that using cross validation results in a much more stable result than using test/train.  The MSE results obtained from test/train are very variable and depend a lot on the data that is split.  This is mitigated by breaking into 5 different folds and obtaining an average.')
--- resampling_lab_classification.py
+++ resampling_lab_classification.py
@@ -1,12 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
-import statistics
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import make_scorer, SCORERS, confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -14,23 +11,23 @@ from sklearn.metrics import make_scorer, SCORERS, confusion_matrix
 rice=pd.read_csv("rice.csv")
 rice_X=rice.drop(columns="CLASS")
 rice_Y=rice['CLASS']
-#Variable names
 rice_class = rice_Y.unique()
 rice_features = rice_X.columns
 gs = GridSearchCV(
     DecisionTreeClassifier(random_state=40),
-    param_grid={"min_samples_split": range(2, 1000)},
+    param_grid={"min_samples_split": range(2, 100)},
     scoring='accuracy',
 )
 gs.fit(rice_X, rice_Y)
 results = gs.cv_results_
-# print(results)
+
+#Show best parameter, and best model score
 print('bestparam',gs.best_params_)
 print('bestscore',gs.best_score_)
-#
+#PRint all of the results from the resulting dictionary
 for key,value in results.items():
     print(key)
-# #
+##Question 2 Using the results of the grid search from question 1, plot the estimates obtained during the search for the best model.
 scores=list(results)[11:12]
 scores = {k: results[k] for k in scores}
 for key,value in (scores.items()):
@@ -39,12 +36,14 @@ for key,value in (scores.items()):
     plt.show()
 
 
-
+#3.1 use train_test_split to build a training set and a testing set with 30 % of the original data for testing;
 Xtrain, Xval, ytrain, yval = train_test_split(rice_X, rice_Y,
                                               train_size=0.7, random_state=42, shuffle=True)
+
+#3.2 Use GridSearchCV to build an optimal decision tree by 5-fold cross-validation on the training set, optimizing the min_samples_split parameter;
 gs = GridSearchCV(
     DecisionTreeClassifier(random_state=40),
-    param_grid={"min_samples_split": range(2, 1000)},
+    param_grid={"min_samples_split": range(2, 100)},
     scoring='accuracy',
     cv=5
 )
@@ -52,15 +51,19 @@ gs.fit(Xtrain, ytrain)
 results = gs.cv_results_
 scores=list(results)[11:13]
 scores = {k: results[k] for k in scores}
-# for key,value in (scores.items()):
-#     plt.plot(value)
-#     plt.title(key)
-#     plt.show()
+for key,value in (scores.items()):
+    plt.plot(value)
+    plt.title(key)
+    plt.show()
 scores=list(results)[11:14]
 scores = {k: results[k] for k in scores}
 print('bestparam',gs.best_params_)
 print('bestscore',gs.best_score_)
 
+#3.3 Compare the performances predicted on the test set with the one obtained by 5-fold CV for the best parameters (available in the best_score_ attribute of the result of GridSearchCV).
+
+
+#Question 4: Estimate the future performances of the tree by computing and printing its confusion matrix and its accuracy on the test set.
 y_pred = gs.best_estimator_.predict(Xval)
 print(confusion_matrix(yval, y_pred))
 
@@ -69,10 +72,11 @@ for i in range(1,20):
                                                   train_size=0.7, random_state=i, shuffle=True)
     gs = GridSearchCV(
         DecisionTreeClassifier(random_state=40),
-        param_grid={"min_samples_split": range(2, 200)},
+        param_grid={"min_samples_split": range(2, 100)},
         scoring='accuracy',
         cv=5
     )
     gs.fit(Xtrain, ytrain)
     print('bestparam for ',i,'random state:', gs.best_params_)
     print('bestscore for ',i,'random state:', gs.best_score_)
+print('By changing the random state and comparing the best parameter and score for each random state, I conclude that there is a small amount of variability resulting from the split sample approach. There is a small negative effect on the stability of this procedure')
