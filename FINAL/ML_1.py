406961a4afd922e0d0d98f067b86c77570f3f12f
jlessoff <jlessoff@gmail.com>
2022-01-18 12:39:36 +0100

    January 18 joined more data

--- FINAL/ML_1.py
+++ FINAL/ML_1.py
@@ -28,78 +28,124 @@ latlong=pd.read_csv("city_loc.csv")
 citypop=pd.read_csv("city_pop.csv")
 regions=pd.read_csv("regions.csv")
 club=pd.read_csv("code_CLUB.csv")
+pcsesemap=pd.read_csv("pcsese2017-map.csv")
+code_job=pd.read_csv('code_Job_42.csv')
+code_job_desc=pd.read_csv('code_job_desc.csv')
 
 
+print(code_job)
 #merge data to get all fields relating to jobs,sports, city information
 learn=pd.merge(learn,learn_jobs,on='UID',how='left')
 learn=pd.merge(learn,learn_sport,on='UID',how='left')
 learn=pd.merge(learn,city_admin,on='INSEE_CODE',how='left')
 learn=pd.merge(learn,departments,on='dep',how='left')
+learn=pd.merge(learn,regions,on='REG',how='left')
 learn=pd.merge(learn,latlong,on='INSEE_CODE',how='left')
 learn=pd.merge(learn,citypop,on='INSEE_CODE',how='left')
-learn=pd.merge(learn,regions,on='REG',how='left')
 learn=pd.merge(learn,club,left_on='CLUB',right_on='Code',how='left')
+learn= learn.drop(columns='Code')
+learn=pd.merge(learn,code_job,left_on='Job_42',right_on='Code',how='left')
+learn= learn.drop(columns='Code')
+learn=pd.merge(learn,code_job_desc,left_on='job_desc',right_on='Code',how='left')
+learn=pd.merge(learn,pcsesemap,left_on='Code',right_on='N3',how='left')
+
 learn['club_indicator']= np.where(learn['Categorie'].isna(),0,1)
 learn['emp_status_indicator']= np.where(learn['ACTIVITY_TYPE']=='type1-1',0,1)
-learn['Is_student']= np.where(learn['Is_student']=='FALSE',0,1)
+learn['Is_student']= np.where(learn['Is_student']==False,0,1)
 
 learn=learn.set_index('UID')
 learn= learn.fillna(0)
+
+
 
 #import test data
 test=pd.read_csv("test_dataset.csv")
 test_jobs=pd.read_csv("test_dataset_job.csv")
 test_sport=pd.read_csv("test_dataset_sport.csv")
 test_emp=pd.read_csv("test_dataset_Emp.csv")
-test=pd.merge(test,learn_jobs,on='UID',how='left')
-test=pd.merge(test,learn_sport,on='UID',how='left')
+test=pd.merge(test,test_jobs,on='UID',how='left')
+test=pd.merge(test,test_sport,on='UID',how='left')
 test=pd.merge(test,city_admin,on='INSEE_CODE',how='left')
 test=pd.merge(test,departments,on='dep',how='left')
+test=pd.merge(test,regions,on='REG',how='left')
 test=pd.merge(test,latlong,on='INSEE_CODE',how='left')
 test=pd.merge(test,citypop,on='INSEE_CODE',how='left')
-test=pd.merge(test,regions,on='REG',how='left')
 test=pd.merge(test,club,left_on='CLUB',right_on='Code',how='left')
-test['Is_student']= np.where(test['Is_student']=='FALSE',0,1)
+test= test.drop(columns='Code')
+test=pd.merge(test,code_job,left_on='Job_42',right_on='Code',how='left')
+test= test.drop(columns='Code')
+test=pd.merge(test,code_job_desc,left_on='job_desc',right_on='Code',how='left')
+test=pd.merge(test,pcsesemap,left_on='Code',right_on='N3',how='left')
+
 test['club_indicator']= np.where(test['Categorie'].isna(),0,1)
-test=test.set_index('UID')
 test['emp_status_indicator']= np.where(test['ACTIVITY_TYPE']=='type1-1',0,1)
-test=test.fillna(0)
+test['Is_student']= np.where(test['Is_student']==False,0,1)
 
 
-#get quick encoding to make correlation heatmap
-
-# label = preprocessing.LabelEncoder()
-# onehotlabels = learn.apply(label.fit_transform)
-# plt.figure(figsize=(16, 6))
-# heatmap = sns.heatmap(onehotlabels.corr())
-# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12);
-# plt.show()
-#
-# heatmap = sns.heatmap(onehotlabels.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
-# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':5}, pad=12);
-# plt.show()
+test=test.set_index('UID')
+test= test.fillna(0)
 
 
-#drop perfectly correlated variables
-test= test.drop(columns=['Nom fédération',"Nom catégorie",'Code','X','Y','INSEE_CODE'])#
-learn= learn.drop(columns=['Nom fédération',"Nom catégorie",'Code','X','Y','INSEE_CODE'])#
-
-
-#save df
-learn.to_csv('learn.csv')
-# create encoding
-cat_vars=['FAMILTY_TYPE','Is_student','Sex','Employee_count','Job_42','DEGREE','ACTIVITY_TYPE','job_condition','Job_category','Terms_of_emp','economic_sector','JOB_DEP','job_desc','Nom de la commune','Nom du département','city_type','REG','Nom de la région','employer_category']
+cat_vars=['Nom fédération',"Nom catégorie",'Code','N3','N2','N1','INSEE_CODE','dep','FAMILTY_TYPE','Sex','Employee_count','Job_42','DEGREE','ACTIVITY_TYPE','job_condition','Job_category','Terms_of_emp','economic_sector','JOB_DEP','job_desc','Nom de la commune','Nom du département','city_type','REG','Nom de la région','employer_category','Categorie']
 
 learn[cat_vars] = learn[cat_vars].astype(str)
-cont_vars=['target','emp_status_indicator','AGE_2018','Working_hours','Pay','inhabitants','Lat','Long','X','Y','club_indicator']
+cont_vars=['target','Is_student','emp_status_indicator','AGE_2018','Working_hours','Pay','inhabitants','Lat','Long','X','Y','club_indicator']
+#
+#
+#make 'na' category
+learn[cat_vars]= np.where(learn[cat_vars]==0,'na',learn[cat_vars])
+test[cat_vars]= np.where(test[cat_vars]==0,'na',test[cat_vars])
+
+#get quick encoding to make correlation heatmap
+label = preprocessing.LabelEncoder()
+onehotlabels_cat = learn[cat_vars].apply(label.fit_transform)
+plt.figure(figsize=(16, 6))
+
+
+# heatmap = sns.heatmap(onehotlabels_cat.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':5}, pad=4);
+# plt.show()
+#
+#
+# onehotlabels_cont = learn[cont_vars].apply(label.fit_transform)
+# heatmap = sns.heatmap(onehotlabels_cont.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':5}, pad=4);
+# plt.show()
+
+# #drop perfectly correlated variables
+test= test.drop(columns=['Code','INSEE_CODE','N3','N2','INSEE_CODE','X','Y'])#
+learn= learn.drop(columns=['Code','INSEE_CODE','N3','N2','INSEE_CODE','X','Y'])#
+
+cat_vars=['Nom fédération',"Nom catégorie",'N1','dep','FAMILTY_TYPE','Sex','Employee_count','Job_42','DEGREE','ACTIVITY_TYPE','job_condition','Job_category','Terms_of_emp','economic_sector','JOB_DEP','job_desc','Nom de la commune','Nom du département','city_type','REG','Nom de la région','employer_category','Categorie']
+cont_vars=['target','Is_student','emp_status_indicator','AGE_2018','Working_hours','Pay','inhabitants','Lat','Long','club_indicator']
+onehotlabels_cat = learn[cat_vars].apply(label.fit_transform)
+onehotlabels_cont = learn[cont_vars].apply(label.fit_transform)
+#
+# heatmap = sns.heatmap(onehotlabels_cat.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':5}, pad=4);
+# plt.show()
+#
+#
+# onehotlabels_cont = learn[cont_vars].apply(label.fit_transform)
+# heatmap = sns.heatmap(onehotlabels_cont.corr(), vmin=-1, vmax=1, annot=True, cmap='BrBG')
+# heatmap.set_title('Correlation Heatmap', fontdict={'fontsize':5}, pad=4);
+# plt.show()
+
+
+#
+#
+
+#
+
+# #save df
+# learn.to_csv('learn.csv')
+# # create encoding
+
 OH_encoder = OneHotEncoder(sparse=False ,handle_unknown='ignore')
 encoded_columns_learn =    OH_encoder.fit_transform(learn[cat_vars])
 # encoded_columns_test =    OH_encoder.transform(test[cat_vars])
 cont_learn=learn[cont_vars].to_numpy()
 processed_data = np.concatenate([cont_learn, encoded_columns_learn], axis=1)
-print(processed_data)
-print(len(cat_vars))
-print(len(cont_vars))
 
 Y=processed_data[:,0]
 X=processed_data[:,2:]
@@ -109,7 +155,6 @@ group_kfold.get_n_splits(X, Y, Group)
 print(group_kfold)
 
 
-# X=[row[1,:] for row in processed_data]
 
 
 ##CREATE ONE HOT ENCODING
---
+++ FINAL/ML_2.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import GroupKFold
+import pandas as pd
+from ML_1 import encoded_columns_learn
+print(encoded_columns_learn)
